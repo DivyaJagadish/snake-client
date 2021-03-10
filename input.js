c56490571834d325885bbe2aae969d11e689e87e
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-10 23:43:56 +0000

    snake mov ing in all directions using wasd

--- input.js
+++ input.js
@@ -3,13 +3,23 @@ const handleUserInput  =function (key) {
     process.exit();
   }
 };
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (key) => {
-    
+    if(key === "w"){
+    connection.write('Move: up' );
+    }else if (key === "a") {
+      connection.write('Move: left' );
+    }else if (key === "s"){
+      connection.write('Move: right');
+    }else if(key === "d"){
+      connection.write('Move: down')
+    }
     handleUserInput(key);
   });
   return stdin;
