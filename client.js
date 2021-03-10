efb362e8835d2a8ccf05b9d99e159c7264f62873
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-10 22:26:30 +0000

    All correctes

--- client.js
+++ client.js
@@ -8,16 +8,19 @@ const connect = function() {
     host: '135.23.222.131',
     port: 50542
   });
+  conn.on("data",function(message){
+    console.log("Received :", message)
+ })
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on("connect",function(){
   console.log("Successfully connected to game server");
+  conn.write('Name: ABC');
+  // conn.write('Move: up' );
   })
-  conn.write("Name:DJ1");
-conn.on("data",function(message){
-   console.log("Received :", message)
-})
-  // return conn;
+  
+
+  return conn;
 }
 
 
